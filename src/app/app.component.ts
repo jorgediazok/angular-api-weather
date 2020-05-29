c273d43dba5318f3a42f784a912a2a2b2c5685c0
Jorge <jorgediazok@gmail.com>
2020-05-29 03:18:44 -0300

    weather service ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-weather-app';
+export class AppComponent implements OnInit {
+  constructor(private weatherService: WeatherService) {}
+  ngOnInit() {}
 }
---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  apiKey = '6d42394a6467a3275912ccc02f5bd749';
+  URI: string = '';
+
+  constructor(private http: HttpClient) {
+    this.URI = `https://api.openweathermap.org/data/2.5/weather?appid=${this.apiKey}&q=`;
+  }
+
+  getWeather(cityName: string, countryCode: string) {
+    return this.http.get(`${this.URI}${cityName},${countryCode}`);
+  }
+}
